2e3c4d1087f0a7becec0f893b936fdbd6be79e31
pasbec <p.b-dev+mfi-mpower@mailbox.org>
2023-01-11 11:09:13 +0100

    Make use of async context manager

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # mfi-mpower-async-api
+
+## API
+https://community.ui.com/questions/mPower-mFi-Switch-and-mFi-In-Wall-Outlet-HTTP-API/824c1c63-b7e6-44ed-b19a-f1d68cd07269
+
+## Usage example with internal session
+```python
+import asyncio
+
+from mfi_mpower import mFimPowerDevice
+
+async def main():
+
+    host = "mympower"
+    username = "admin"
+    password = "correcthorsebatterystaple"
+    use_ssl = True
+    verify_ssl = True
+
+    async with mFimPowerDevice(host, username, password, use_ssl, verify_ssl) as device:
+
+        # Show port data
+        print(await device.data)
+
+        # Switch first port off
+        await device.set(1, False)
+        
+        # Sleep 5 seconds
+        await asyncio.sleep(5)
+
+        # Toggle first port
+        await device.toggle(1)
+
+asyncio.run(main())
+```
+
+## Usage example with external session
+```python
+import aiohttp
+import asyncio
+
+from mfi_mpower import mFimPowerDevice
+
+async def main():
+
+    host = "mympower"
+    username = "admin"
+    password = "correcthorsebatterystaple"
+    use_ssl = True
+    verify_ssl = True
+
+    async with aiohttp.ClientSession() as session:
+        async with mFimPowerDevice(host, username, password, use_ssl, verify_ssl, session) as device:
+
+            # Show port data
+            print(await device.data)
+
+            # Switch first port off
+            await device.set(1, False)
+            
+            # Sleep 5 seconds
+            await asyncio.sleep(5)
+
+            # Toggle first port
+            await device.toggle(1)
+
+asyncio.run(m
--- mfi_mpower.py
+++ mfi_mpower.py
@@ -7,22 +7,37 @@ import time
 class mFimPowerDevice:
     """Async API for Ubiquiti mFi mPower devices as explained here: https://community.ui.com/questions/mPower-mFi-Switch-and-mFi-In-Wall-Outlet-HTTP-API/824c1c63-b7e6-44ed-b19a-f1d68cd07269"""
 
-    _ssl = False
-    _authenticated = False
-    _time = time.time()
+
+    _session = None
+    _authenticated = None
+    _time = None
     _data = None
+
+    async def __aenter__(self):
+        await self.login()
+        return self
+
+    async def __aexit__(self, *excinfo):
+        await self.logout()
 
     def __init__(
         self,
-        websession: ClientSession,
         host: str,
         username: str,
         password: str,
         use_ssl: bool = True,
         verify_ssl: bool = True,
-        cache_time: int = 0
+        cache_time: int = 0,
+        session: ClientSession = None
     ):
-        self.websession = websession
+        if session is None:
+            self._session = True
+        else:
+            self.session = session
+            self._session = False
+
+        self._authenticated = False
+        self._time = time.time()
 
         self.host = host
         self.username = username
@@ -30,36 +45,42 @@ class mFimPowerDevice:
 
         if use_ssl:
             # Ubiquiti mFi mPower Devices use ancient OpenSSL (1.0.0g 18 Jan 2012)
-            self._ssl = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-            self._ssl.set_ciphers("AES256-SHA:AES128-SHA")
-            self._ssl.load_default_certs()
-            self._ssl.verify_mode = ssl.CERT_REQUIRED if verify_ssl else ssl.CERT_NONE
+            self.ssl = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+            self.ssl.set_ciphers("AES256-SHA:AES128-SHA")
+            self.ssl.load_default_certs()
+            self.ssl.verify_mode = ssl.CERT_REQUIRED if verify_ssl else ssl.CERT_NONE
+        else:
+            self.ssl = False
 
         self.cache_time = cache_time
 
     @property
     def url(self) -> str:
         """Device URL"""
-        if self._ssl:
+        if self.ssl:
             return f"https://{self.host}"
         else:
             return f"http://{self.host}"
 
     async def login(self) -> None:
         """Login method"""
-        # Initial get request is required (cookie?)
-        await self.websession.get(
+
+        if self._session:
+            self.session = ClientSession()
+
+        # Initial get request is required (cookie)
+        await self.session.get(
             self.url,
-            ssl=self._ssl
+            ssl=self.ssl
         )
 
-        response = await self.websession.post(
+        response = await self.session.post(
             f"{self.url}/login.cgi",
             data={
                 "username": self.username,
                 "password": self.password
             },
-            ssl=self._ssl
+            ssl=self.ssl
         )
 
         if not response.status == 200:
@@ -73,9 +94,9 @@ class mFimPowerDevice:
 
     async def logout(self) -> None:
         """Logout method"""
-        response = await self.websession.post(
+        response = await self.session.post(
             f"{self.url}/logout.cgi",
-            ssl=self._ssl
+            ssl=self.ssl
         )
 
         if not response.status == 200:
@@ -83,14 +104,18 @@ class mFimPowerDevice:
 
         self._authenticated = False
 
+        if self._session:
+            await self.session.close()
+            self.session = None
+
     async def config(self) -> str:
         """Configuration retrieval"""
         if not self._authenticated:
             await self.login()
         
-        response = await self.websession.get(
+        response = await self.session.get(
             f"{self.url}/cfg.cgi",
-            ssl=self._ssl
+            ssl=self.ssl
         )
 
         if not response.status == 200:
@@ -105,9 +130,9 @@ class mFimPowerDevice:
 
         if self._data is None or (time.time() - self._time) > self.cache_time:
         
-            response = await self.websession.get(
+            response = await self.session.get(
                 f"{self.url}/sensors",
-                ssl=self._ssl
+                ssl=self.ssl
             )
 
             if not response.status == 200:
@@ -137,29 +162,29 @@ class mFimPowerDevice:
         return len(data)
 
     async def get(self, port: int) -> dict:
-        """Get data for indivial ports"""
+        """Get data for individual ports"""
         data = await self.data
         return data[port - 1]
 
     async def set(self, port: int, output: bool) -> None:
-        """Set output for indivial ports"""
+        """Set output for individual ports"""
         if port < 1:
             raise ValueError(f"Port number {port} is too small")
 
         if port > await self.ports:
             raise ValueError("Port number {port} is too large")
         
-        response = await self.websession.put(
+        response = await self.session.put(
             f"{self.url}/sensors/{port}",
             data={"output": int(output)},
-            ssl=self._ssl
+            ssl=self.ssl
         )
 
         if not response.status == 200:
             raise aiohttp.ClientError(f"Setting failed with status {response.status}")
 
     async def toggle(self, port: int) -> None:
-        """Toggle output for indivial ports"""
+        """Toggle output for individual ports"""
         port_data = await self.get(port)
         output = not bool(port_data["output"])
         await self.set(port=port, output=output)
