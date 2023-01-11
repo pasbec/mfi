89eb874dfe6f46c53424f281cd08466c6550d3e4
pasbec <p.b-dev+mfi-mpower@mailbox.org>
2023-01-11 11:09:19 +0100

    Make MPowerBoard and MPowerEntity available in mfi_mpower namespace

--- src/mfi_mpower/__init__.py
+++ src/mfi_mpower/__init__.py
@@ -1,6 +1,9 @@
 """This is the mFi mPower module."""
+from __future__ import annotations
+
+from .board import MPowerBoard
 from .device import MPowerDevice
-from .entities import MPowerSensor, MPowerSwitch
+from .entities import MPowerEntity, MPowerSensor, MPowerSwitch
 from .exceptions import *
 
 __version__ = "1.2.0"
